bb4189cdab74aa582170930582bf026b4fc77fab
Yari Tkachuk <coderpyt@gmail.com>
2025-09-01 11:12:06 +0200

    Feat: Use vector retriever in a RAG pipeline

--- genai-fundamentals/vector_rag.py
+++ genai-fundamentals/vector_rag.py
@@ -5,6 +5,9 @@ load_dotenv()
 from neo4j import GraphDatabase
 from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
 from neo4j_graphrag.retrievers import VectorRetriever
+from neo4j_graphrag.llm import OpenAILLM
+from neo4j_graphrag.generation import GraphRAG
+
 
 # Connect to Neo4j database
 driver = GraphDatabase.driver(
@@ -20,17 +23,33 @@ embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 # Create retriever
 retriever = VectorRetriever(
-    driver,
+    driver=driver,
     index_name="moviePlots",
     embedder=embedder,
     return_properties=["title", "plot"],
 )
 
 # Create the LLM
+llm = OpenAILLM(
+    model_name="gpt-4o", 
+    model_params={"temperature": 1}
+)
 
 # Create GraphRAG pipeline
+rag = GraphRAG(
+    llm=llm,
+    retriever=retriever,
+)
 
-# Search 
+# Search
+query_text = "Find me movies about building AI robots"
+
+response = rag.search(
+    query_text=query_text, 
+    retriever_config={"top_k": 5}
+)
+
+print(response.answer)
 
 # CLose the database connection
 driver.close()
